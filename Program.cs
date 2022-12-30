938c0126a2d6d23f848b92173630f76238c987f4
kadriamunir <121021383+kadriamunir@users.noreply.github.com>
2022-12-30 08:13:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter first num : ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter second num : ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("the perfect numbers are : ");
+
+            int sum = 0;
+
+            for (int x = n1; x <= n2; x++)
+            {
+                for (int y = 1; y < x; y++)
+                {
+                    if (x == 1)
+                        continue;
+
+                    if (x % y == 0)
+                        sum += y;
+
+                }
+                if (sum == x)
+                    Console.WriteLine(x);
+
+                sum = 0;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
